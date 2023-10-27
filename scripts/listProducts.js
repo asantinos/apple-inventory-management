f309c5c7e36ce2c31953da4dfcbd761f236e4df6
Alex Santos <aleexsantos1509@gmail.com>
2023-10-27 12:24:58 +0200

    Added total inventory text. Created functional delete button. Styled sidebar card

--- scripts/listProducts.js
+++ scripts/listProducts.js
@@ -1,7 +1,7 @@
 import { inventory } from "./products.js";
 
 export const listProducts = () => {
-    const inventoryData = document.getElementById("inventory-data");
+    const inventoryData = document.getElementById("inventory-table-data");
 
     while (inventoryData.firstChild) {
         inventoryData.removeChild(inventoryData.firstChild);
@@ -10,13 +10,11 @@ export const listProducts = () => {
     inventory.forEach((item) => {
         const dataRow = inventoryData.insertRow();
 
-        const idCell = dataRow.insertCell(0);
-        const nameCell = dataRow.insertCell(1);
-        const quantityCell = dataRow.insertCell(2);
-        const priceCell = dataRow.insertCell(3);
-        const actionsCell = dataRow.insertCell(4);
+        const nameCell = dataRow.insertCell(0);
+        const quantityCell = dataRow.insertCell(1);
+        const priceCell = dataRow.insertCell(2);
+        const actionsCell = dataRow.insertCell(3);
 
-        idCell.innerText = item.id;
         nameCell.innerText = item.name;
         quantityCell.innerText = item.quantity;
         priceCell.innerText = `$${item.price}`;
@@ -35,7 +33,16 @@ export const listProducts = () => {
         const deleteButton = document.createElement("button");
         deleteButton.innerText = "DELETE";
         deleteButton.addEventListener("click", () => {
-            // Logica para eliminar un producto
+            if (confirm(`Are you sure you want to delete ${item.name}?`)) {
+                const index = inventory.findIndex(
+                    (product) => product.id === item.id
+                );
+
+                if (index !== -1) {
+                    inventory.splice(index, 1);
+                    dataRow.remove();
+                }
+            }
         });
         actionsCell.appendChild(deleteButton);
     });
